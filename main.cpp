b6e8be5e22b0588bfa2c7232fb112bc46851901b
Annie Bayes <akbayes@nic.edu>
2025-03-26 20:42:37 +0000

    added add function

--- main.cpp
+++ main.cpp
@@ -1,10 +1,17 @@
 #include <iostream>
 using namespace std;
 
+int add(int a, int b);
+
 int main()
 {
     cout << "Hello World!" << endl;
     return 0;
 
 
+}
+
+int add(int a, int b)
+{
+    return a + b;
 }
