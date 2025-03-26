55ef88fc2facae0dd83014c99db9c944af0f90fc
Annie Bayes <akbayes@nic.edu>
2025-03-26 13:48:59 -0700

    added say function

--- main.cpp
+++ main.cpp
@@ -1,11 +1,20 @@
 #include <iostream>
 using namespace std;
 
+void say(string s);
+
 int main()
 {
     cout << "Greeting...\n";
     cout << "Hello World!" << endl;
     return 0;
 
+
+}
+
+void say(string s)
+{
+
+    cout << s << endl;
 
 }
