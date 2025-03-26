72556b18db740e213093b2469030e1b2711c1e33
Annie Bayes <akbayes@nic.edu>
2025-03-26 14:08:20 -0700

    undid what was done

--- main.cpp
+++ main.cpp
@@ -1,21 +1,17 @@
 #include <iostream>
 using namespace std;
 
-void say(string s);
 int add(int a, int b);
 
 int main()
 {
-
+    cout << "Greeting...\n";
+    cout << "Hello World!" << endl;
     return 0;
 
 
 }
 
-void say(string s)
-{
-    cout << s << endl;
-}
 int add(int a, int b)
 {
     
